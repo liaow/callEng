1399e52ba17e3898ac0e7b1f7e51a4b92c773b5c
liaowu <cs.liaow@gmail.com>
2014-07-27 00:28:41 +0800

    Update callEng.sh
    修订

--- callEng.sh
+++ callEng.sh
@@ -1,35 +1,120 @@
 #!/bin/bash
-rulehome=/data11/airwdeng/reward_run
-bindir=$rulehome/svr
-logDir=$rulehome/log/shelllog
+#####################################################
+#             引擎调用通用shell                     #
+#              liaow@gmail.com                      #
+#                                                   #
+#  更新历史                                         #
+#  2014-07-26  创建脚本                             #
+#  2014-07-27  支撑不指定地市或者情况从数据库取     #
+#                                                   #
+#  待演进功能                                       #
+#  1、地市和业务有效性校验                          #
+#  2、指定节点单独运行支撑                          #
+#  3、检测RWD_HOME是否定义，否则find来定义          #
+#####################################################
 
-if [ $# -lt 3 -a $# -lt 4 -a  $# -lt 5 -a $# -lt 6 ]; then
-        echo "input param err,please type cmd like (calMaxCnt,calMode,settMonth,eparchyCode,ruleTypeCode,nodeSeq)"
-        exit -1;
+#自适应定义SHELL_HOME目录
+SHELL_HOME=$(cd `dirname $0`; pwd)
+LOG_PATH=$SHELL_HOME/log
+
+#没有指定地州或者业务时是否使用数据库自动读取
+#使用Wmsys.Wm_Concat函数需要oracle10.2版本以上
+DB_ON=Y
+CONNSTR="rwd_busi/airwddb+1@AIRWDDB_124_122"
+
+startTime=`date +%s`
+startTimeStr=`date +"%Y%m%d%H%M%S"`
+
+#shell call日志
+if [ ! -d "$LOG_PATH" ]; then
+    mkdir $LOG_PATH
 fi
 
-maxCnt=$1
-
-echo "input param maxCnt="$maxCnt
-
-if [ "$maxCnt" -ne 1 ] && [ "$maxCnt" -ne 2 ] && [ "$maxCnt" -ne 3 ]; then
-	echo "maxCnt value need equals 1 or 2 or 3"
-	exit -1;
+callLogFile=${LOG_PATH}/call_${SETT_MONTH}_${startTimeStr}.log
+if [ -f $callLogFile ]; then
+        rm $callLogFile
 fi
 
-calMode=$2
+echo `date +"%Y-%m-%d %H:%M:%S"`"###################################################################" >> $callLogFile
+echo `date +"%Y-%m-%d %H:%M:%S"`"#  DO PLEASE CHECK ENV HAS A RWD_HOME , AND MAKE IT TAKE EFFECT!  #" >> $callLogFile
+echo `date +"%Y-%m-%d %H:%M:%S"`"#       请检查环境变量RWD_HOME是否配置，否则配置并使其生效！      #" >> $callLogFile
+echo `date +"%Y-%m-%d %H:%M:%S"`"###################################################################" >> $callLogFile
 
-if [ "$calMode" -ne 1 ] && [ "$calMode" -ne 2 ] && [ "$calMode" -ne 3 ] && [ "$calMode" -ne 4 ]; then
-	echo "calMode value need equals 1 or 2 or 3 or 4 means null or m or y or ym"
-	exit -1;
+usage ()
+{
+        echo "Usage : Script -[option]"
+        echo "  p,  program_name,    必选,  程序名 CalApp 或者 TransTL"
+        echo "  e,  eparchy_code,    必选,  需要处理的地州编码(4位)"
+        echo "  c,  sett_month,      必选,  需要处理的月份[-1:上月;0:本月;其他(yyyymm):指定账期]"
+        echo "  r,  rule_type_code,  必选,  类型,如4100"
+        echo "  i,  ini_file,        可选,  指定配置文件(cfg.ini)"
+        echo "  t,  node_id,         可选,  指定节点"
+        echo "  m,  max_active,      可选,  最大并发数"
+        echo "  o,  other_params,    可选,  其他引擎可支持参数"
+        exit
+}
+
+#参数解析
+while getopts ":p:e:c:r:i:t:m:o:" opt
+do
+        case $opt in
+        p)
+                PROGRAM_NAME="$OPTARG"
+                echo `date +"%Y-%m-%d %H:%M:%S"`" PROGRAM_NAME=" "$OPTARG" >> $callLogFile
+                ;;
+        e)
+                EPARCHY_CODE="$OPTARG"
+                echo `date +"%Y-%m-%d %H:%M:%S"`" EPARCHY_CODE=" "$OPTARG" >> $callLogFile
+                ;;
+        c)
+                SETT_MONTH="$OPTARG"
+                echo `date +"%Y-%m-%d %H:%M:%S"`" SETT_MONTH=" "$OPTARG" >> $callLogFile
+                ;;
+        r)
+                RULE_TYPE_CODE="$OPTARG"
+                echo `date +"%Y-%m-%d %H:%M:%S"`" RULE_TYPE_CODE=" "$OPTARG" >> $callLogFile
+                ;;
+        i)
+                CONFIG_FILE="$OPTARG"
+                echo `date +"%Y-%m-%d %H:%M:%S"`" CONFIG_FILE=" "$OPTARG" >> $callLogFile
+                ;;
+        t)
+                NODE_ID="$OPTARG"
+                echo `date +"%Y-%m-%d %H:%M:%S"`" NODE_ID=" "$OPTARG" >> $callLogFile
+                ;;
+        m)
+                MAX_ACTIVE="$OPTARG"
+                echo `date +"%Y-%m-%d %H:%M:%S"`" MAX_ACTIVE=" "$OPTARG" >> $callLogFile
+                ;;
+        o)
+                OTHER_PARAMS="$OPTARG"
+                echo `date +"%Y-%m-%d %H:%M:%S"`" OTHER_PARAMS=" "$OPTARG" >> $callLogFile
+                ;;
+        ?) usage;;
+        esac
+done
+
+#检查参数必填项
+if [ x"$PROGRAM_NAME" = x ] ; then
+        usage
+fi
+if [ x"$EPARCHY_CODE" = x ] ; then
+        usage
+fi
+if [ x"$SETT_MONTH" = x ] ; then
+        usage
+fi
+if [ x"$RULE_TYPE_CODE" = x ] ; then
+        usage
 fi
 
-echo "input param calMode="$calMode
+#设置默认并发
+if [ x"$MAX_ACTIVE" = x ] ; then
+        MAX_ACTIVE=1
+fi
 
-settMonth=$3
-echo "input param settMonth="$settMonth
-
-if [ $settMonth -eq -1 ]; then
+#处理账期
+if [ $SETT_MONTH -eq -1 ]; then
 	year=`date +"%Y"`
 	month=`date +"%m" `
 	let "lastMonth=month-1"
@@ -39,132 +124,117 @@ if [ $settMonth -eq -1 ]; then
 	elif [ $lastMonth -lt 10 ]; then
 	  lastMonth="0"$lastMonth
 	fi
-	settMonth=$year$lastMonth
+	SETT_MONTH=$year$lastMonth
+fi
+if [ $SETT_MONTH -eq 0 ]; then
+	SETT_MONTH=`date +"%Y%m"`
 fi
 
-if [ $settMonth -eq 0 ]; then
-	settMonth=`date +"%Y%m"`
+#多参数输入处理
+#从数据库读业务编码
+if [ "$DB_ON" = "Y" ] ; then
+    TMP_RULE_TYPE_CODE=`sqlplus -silent $CONNSTR <<EOF  
+    SET PAGESIZE 0 FEEDBACK OFF VERIFY OFF HEADING OFF ECHO OFF  
+    Select Wmsys.Wm_Concat(Data_Id) ruleType From Rwd_Core_Static a Where a.Type_Id = 'RWD_RULE_TYPE_CODE';
+    EXIT;  
+EOF`
 fi
+echo `date +"%Y-%m-%d %H:%M:%S"`" RULE_TYPE_CODE equals ${RULE_TYPE_CODE} ,use database =${DB_ON} , TMP_RULE_TYPE_CODE : "$TMP_RULE_TYPE_CODE >>$callLogFile
 
-echo "input param settMonth trans result="$settMonth
-
-if [ ! -d "$logDir/$settMonth" ]; then
-	mkdir "$logDir/$settMonth"
+if [ "$RULE_TYPE_CODE" -eq 0 ] ; then
+    if [ -z "$TMP_RULE_TYPE_CODE" ]; then
+        RULE_TYPE_CODE="4000,4110,4120,4130,4140,4150,4250"
+    else
+        RULE_TYPE_CODE=$TMP_RULE_TYPE_CODE
+    fi
 fi
-startTime=`date +%s`
-startTimeStr=`date +"%Y%m%d%H%M%S"`
-tmpLogDir=$logDir/$settMonth/$startTimeStr
-if [ -d "$tmpLogDir" ]; then
-        rm -r "$tmpLogDir"
+echo `date +"%Y-%m-%d %H:%M:%S"`" RULE_TYPE_CODE equals ${RULE_TYPE_CODE} ,use database =${DB_ON} , current RULE_TYPE_CODE : "$RULE_TYPE_CODE >>$callLogFile
+
+#从数据库读地州编码
+if [ "$DB_ON" = "Y" ] ; then
+    TMP_EPARCHY_CODE=`sqlplus -silent $CONNSTR <<EOF  
+    SET PAGESIZE 0 FEEDBACK OFF VERIFY OFF HEADING OFF ECHO OFF  
+    Select Wmsys.Wm_Concat(Area_Code) areaCode From Rwd_Core_Area a Where a.Area_Level = 20;  
+    EXIT;  
+EOF`
 fi
-mkdir "$tmpLogDir"
-ruleCalLogFile=${tmpLogDir}/ruleCal_${settMonth}_${startTimeStr}.log
-if [ -f $ruleCalLogFile ]; then
-        rm $ruleCalLogFile
+echo `date +"%Y-%m-%d %H:%M:%S"`" EPARCHY_CODE equals ${EPARCHY_CODE} ,use database =${DB_ON} , TMP_EPARCHY_CODE : "$TMP_EPARCHY_CODE >>$callLogFile
+
+if [ "$EPARCHY_CODE" -eq 0 ]; then
+    if [ -z "$TMP_EPARCHY_CODE" ]; then
+        EPARCHY_CODE="0770,0771,0772,0773,0774,0775,0776,0777,0778,0779,0780,0781,0782,0783"
+    else
+        EPARCHY_CODE=$TMP_EPARCHY_CODE
+    fi
 fi
+echo `date +"%Y-%m-%d %H:%M:%S"`" EPARCHY_CODE equals ${EPARCHY_CODE} ,use database =${DB_ON} , current EPARCHY_CODE : "$EPARCHY_CODE >>$callLogFile
 
-echo `date +"%Y-%m-%d %H:%M:%S"`"-------------------------------------------------------------------------------------"
-echo `date +"%Y-%m-%d %H:%M:%S"`"------------------------------------all biz begin----------------startTime="$startTime
-echo `date +"%Y-%m-%d %H:%M:%S"`"-------------------------------------------------------------------------------------"
-
-echo `date +"%Y-%m-%d %H:%M:%S"`"-------------------------------------------------------------------------------------" >>$ruleCalLogFile
-echo `date +"%Y-%m-%d %H:%M:%S"`"------------------------------------all biz begin----------------startTime="$startTime >>$ruleCalLogFile
-echo `date +"%Y-%m-%d %H:%M:%S"`"-------------------------------------------------------------------------------------" >>$ruleCalLogFile
-
-if [ $# -eq 4 ]; then
-        if [ ` expr substr "$4" 1 1 ` -eq 0 ]; then
-                eparchyCodes=$4
-        else
-                ruleTypeCodes=$4
-        fi
-fi
-
-if [ $# -eq 5 ]; then
-        if [ ` expr substr "$4" 1 1 ` -eq 0 ]; then
-                eparchyCodes=$4
-                ruleTypeCodes=$5
-        else
-                ruleTypeCodes=$4
-                nodeSeq=$5
-        fi
-        
-fi
-
-if [ $# -eq 6 ]; then
-        eparchyCodes=$4
-        ruleTypeCodes=$5
-        nodeSeq=$6
-fi
-
-eparchyCodes=`echo $eparchyCodes | sed 's/:/,/g'`
-ruleTypeCodes=`echo $ruleTypeCodes | sed 's/:/,/g'`
-nodeSeq=`echo $nodeSeq | sed 's/:/,/g'`
-echo "input param eparchyCodes="$eparchyCodes
-echo "input param ruleTypeCodes="$ruleTypeCodes
-echo "input param nodeSeq="$nodeSeq
-
-if [ ` expr length "$eparchyCodes" ` -eq 0 ]; then
-        eparchyCodes="0570,0571,0572,0573,0574,0575,0576,0577,0578,0579,0580"
-fi
-
-if [ ` expr length "$ruleTypeCodes" ` -eq 0 ]; then
-        ruleTypeCodes="4000,4110,4120,4130,4140,4150,4230,4240,4250,4260,4270,4280,4290,4310,4330,4340"
-fi
-
-echo `date +"%Y-%m-%d %H:%M:%S"`"------settMonth="$settMonth"----eparchyCodes="$eparchyCodes"-----ruleTypeCodes="$ruleTypeCodes"-----nodeSeq="$nodeSeq
-echo `date +"%Y-%m-%d %H:%M:%S"`"------settMonth="$settMonth"----eparchyCodes="$eparchyCodes"-----ruleTypeCodes="$ruleTypeCodes"-----nodeSeq="$nodeSeq >>$ruleCalLogFile
-
-if [ ` expr index "$eparchyCodes" "," ` -gt 0 ]; then
-        eparchyCodesReplace=`echo $eparchyCodes | sed 's/,/ /g'`
-        eval set -A arrEparchyCodes $eparchyCodesReplace
+#有节点时需要拼-t
+if [ x"${NODE_ID}" = x ]; then
+        NODE_PARAM=""
 else
-        eval set -A arrEparchyCodes $eparchyCodes
-        maxCnt=1
+        NODE_PARAM="-t"${NODE_ID}
+        echo "node param is "${NODE_PARAM}
 fi
 
-if [ ` expr index "$ruleTypeCodes" "," ` -gt 0 ]; then
-        ruleTypeCodesReplace=`echo $ruleTypeCodes | sed 's/,/ /g'`
-        eval set -A arrRuleTypeCodes $ruleTypeCodesReplace
-else
-        eval set -A arrRuleTypeCodes $ruleTypeCodes
+#没有指定config取默认配置
+if [ x"${CONFIG_FILE}" = x ]; then
+        CONFIG_FILE="${RWD_HOME}/config/cal_cfg.ini"
+        echo `date +"%Y-%m-%d %H:%M:%S"`" CONFIG_FILE seems null ,use default config file : "$CONFIG_FILE >>$callLogFile
 fi
 
+echo `date +"%Y-%m-%d %H:%M:%S"`" SETT_MONTH="$SETT_MONTH";EPARCHY_CODE="$EPARCHY_CODE";RULE_TYPE_CODE="$RULE_TYPE_CODE";NODE_PARAM="$NODE_PARAM";CONFIG_FILE="$CONFIG_FILE >>$callLogFile
+
+echo `date +"%Y-%m-%d %H:%M:%S"`"-------------------------------------------------------------------------------------" >>$callLogFile
+echo `date +"%Y-%m-%d %H:%M:%S"`"------------------------------------all biz begin----------------startTime="$startTime >>$callLogFile
+echo `date +"%Y-%m-%d %H:%M:%S"`"-------------------------------------------------------------------------------------" >>$callLogFile
+
+arrEparchyCodes=(${EPARCHY_CODE//,/ })
+arrRuleTypeCodes=(${RULE_TYPE_CODE//,/ })
+
+#shell 程序日志
+programLogFile=${LOG_PATH}/${PROGRAM_NAME}_${SETT_MONTH}_${startTimeStr}.log
+if [ -f $programLogFile ]; then
+        rm $programLogFile
+fi
+
+#切换到引擎目录
+cd $RWD_HOME/bin
+
+#开始处理
 for ruleTypeCode in ${arrRuleTypeCodes[@]}; do
         for eparchyCode in ${arrEparchyCodes[@]}; do
-                AMOUNT=` ps -ef |grep executeCalApp |grep -v grep |sort -u |wc -l `;
-                AMOUNT2=` ps -ef |grep executeCalApp |grep "$eparchyCode" |grep -v grep |sort -u |wc -l `;
-                while [ "$AMOUNT" -ge ${maxCnt} ] || [ "$AMOUNT2" -eq 1 ]; do
-                        sleep 10
-                        AMOUNT=` ps -ef |grep executeCalApp |grep -v grep |sort -u |wc -l `;
-                        AMOUNT2=` ps -ef |grep executeCalApp |grep "$eparchyCode" |grep -v grep |sort -u |wc -l `;
+                AMOUNT=` ps -ef |grep "$PROGRAM_NAME" | grep -v $0 | grep -v grep |sort -u |wc -l `;
+                AMOUNT2=` ps -ef |grep "$PROGRAM_NAME" | grep -v $0 | grep "$eparchyCode" |grep -v grep |sort -u |wc -l `;
+                #echo "ps -ef,AMOUNT="${AMOUNT}",AMOUNT2="${AMOUNT2}",MAX_ACTIVE="${MAX_ACTIVE}
+                while [ "$AMOUNT" -ge ${MAX_ACTIVE} ] || [ "$AMOUNT2" -eq 1 ]; do
+                    #echo "sleep ..."
+                    sleep 10
+                    AMOUNT=` ps -ef |grep "$PROGRAM_NAME" | grep -v $0 | grep -v grep |sort -u |wc -l `;
+                    AMOUNT2=` ps -ef |grep "$PROGRAM_NAME" | grep -v $0 | grep "$eparchyCode" |grep -v grep |sort -u |wc -l `;
+                    #echo "ps -ef ,AMOUNT="${AMOUNT}",AMOUNT2="${AMOUNT2}",MAX_ACTIVE="${MAX_ACTIVE}
                 done
                 sleep 10
-                echo "begin execute CalApp,param[calMode|settMonth|eparchyCode|ruleTypeCode]=["$calMode"|"$settMonth"|"$eparchyCode"|"$ruleTypeCode"]"
-                if [ "${nodeSeq}" -eq "" ]; then
-                        sh ./executeCalApp.sh $startTimeStr $calMode $settMonth $eparchyCode $ruleTypeCode &
-                else
-                        sh ./executeCalApp.sh $startTimeStr $calMode $settMonth $eparchyCode $ruleTypeCode $nodeSeq &
-                fi
+                echo " begin execute $PROGRAM_NAME,param[sett_month|eparchy_code|rule_type_code|node_id|max_active|config_file|other_params]" >> $callLogFile
+                echo "                                 ["$SETT_MONTH"|"$eparchyCode"|"$ruleTypeCode"|"$NODE_ID"|"$MAX_ACTIVE"|"$CONFIG_FILE"|"$OTHER_PARAMS"]" >> callLogFile
+                #程序调用开始
+                echo " ./"$PROGRAM_NAME" -i"${CONFIG_FILE}" -c"${SETT_MONTH}" -e"${eparchyCode}" -r"${ruleTypeCode}" "${NODE_PARAM}" -"${OTHER_PARAMS} >> $callLogFile
+                ./$PROGRAM_NAME -i${CONFIG_FILE} -c${SETT_MONTH} -e${eparchyCode} -r${ruleTypeCode} ${NODE_PARAM} -${OTHER_PARAMS} >> $programLogFile
         done
 done
 
-AMOUNT=` ps -ef |grep executeCalApp |grep -v grep |sort -u |wc -l `;
+AMOUNT=` ps -ef |grep "$PROGRAM_NAME" | grep -v $0 |grep -v grep |sort -u |wc -l `;
 while [ "$AMOUNT" -ge 1 ]; do
         sleep 10
-        AMOUNT=` ps -ef |grep executeCalApp |grep -v grep |sort -u |wc -l `;
+        AMOUNT=` ps -ef |grep "$PROGRAM_NAME" | grep -v $0 |grep -v grep |sort -u |wc -l `;
 done
 
 endTime=`date +%s`
 allDurationTime=$(($endTime-$startTime))
 
-echo `date +"%Y-%m-%d %H:%M:%S"`"-------------------------------------------------------------------------------------"
-echo `date +"%Y-%m-%d %H:%M:%S"`"-------------------------------------all biz end---------------------endTime="$endTime
-echo `date +"%Y-%m-%d %H:%M:%S"`"---------------------------------------all biz deal completed! cost "$allDurationTime" secs"
-echo `date +"%Y-%m-%d %H:%M:%S"`"-------------------------------------------------------------------------------------"
-
-echo `date +"%Y-%m-%d %H:%M:%S"`"-------------------------------------------------------------------------------------" >>$ruleCalLogFile
-echo `date +"%Y-%m-%d %H:%M:%S"`"-------------------------------------all biz end---------------------endTime="$endTime >>$ruleCalLogFile
-echo `date +"%Y-%m-%d %H:%M:%S"`"---------------------------------------all biz deal completed! cost "$allDurationTime" secs" >>$ruleCalLogFile
-echo `date +"%Y-%m-%d %H:%M:%S"`"-------------------------------------------------------------------------------------" >>$ruleCalLogFile
+echo `date +"%Y-%m-%d %H:%M:%S"`"-------------------------------------------------------------------------------------" >>$callLogFile
+echo `date +"%Y-%m-%d %H:%M:%S"`"-------------------------------------all biz end---------------------endTime="$endTime >>$callLogFile
+echo `date +"%Y-%m-%d %H:%M:%S"`"-------------------------------------all biz deal completed! cost "$allDurationTime" secs" >>$callLogFile
+echo `date +"%Y-%m-%d %H:%M:%S"`"-------------------------------------------------------------------------------------" >>$callLogFile
 echo "ok!"
 exit 0;
