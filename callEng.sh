2eb11d8b68569743acad1b67dc32558dd24f827b
liaowu <cs.liaow@gmail.com>
2014-07-28 15:46:47 +0800

    并发改用统计子脚本(-p程序本身并发)
    并发改用统计子脚本(-p程序本身并发)
    修改多值传递分割方式(半角英文分号)

--- callEng.sh
+++ callEng.sh
@@ -6,11 +6,9 @@
 #  更新历史                                         #
 #  2014-07-26  创建脚本                             #
 #  2014-07-27  支撑不指定地市或者情况从数据库取     #
+#  2014-07-28  并发改用统计子脚本(-p程序本身并发)   #
+#              修改多值传递分割方式(半角英文分号)   #
 #                                                   #
-#  待演进功能                                       #
-#  1、地市和业务有效性校验                          #
-#  2、指定节点单独运行支撑                          #
-#  3、检测RWD_HOME是否定义，否则find来定义          #
 #####################################################
 
 #自适应定义SHELL_HOME目录
@@ -20,7 +18,7 @@ LOG_PATH=$SHELL_HOME/log
 #没有指定地州或者业务时是否使用数据库自动读取
 #使用Wmsys.Wm_Concat函数需要oracle10.2版本以上
 DB_ON=Y
-CONNSTR="USER/PASSWD@TNS_NAME"
+CONNSTR="rwd_busi/airwddb+1@AIRWDDB_124_122"
 
 startTime=`date +%s`
 startTimeStr=`date +"%Y%m%d%H%M%S"`
@@ -30,7 +28,7 @@ if [ ! -d "$LOG_PATH" ]; then
     mkdir $LOG_PATH
 fi
 
-callLogFile=${LOG_PATH}/call_${SETT_MONTH}_${startTimeStr}.log
+callLogFile=${LOG_PATH}/call_${startTimeStr}.log
 if [ -f $callLogFile ]; then
         rm $callLogFile
 fi
@@ -44,9 +42,9 @@ usage ()
 {
         echo "Usage : Script -[option]"
         echo "  p,  program_name,    必选,  程序名 CalApp 或者 TransTL"
-        echo "  e,  eparchy_code,    必选,  需要处理的地州编码(4位)"
+        echo "  e,  eparchy_code,    必选,  需要处理的地州编码(4位)[0表示所有地州,也可以传多个以半角冒号隔开],参考其他说明"
         echo "  c,  sett_month,      必选,  需要处理的月份[-1:上月;0:本月;其他(yyyymm):指定账期]"
-        echo "  r,  rule_type_code,  必选,  类型,如4100"
+        echo "  r,  rule_type_code,  必选,  类型,如4100[0表示所有业务,也可以传多个以半角冒号隔开],参考其他说明"
         echo "  i,  ini_file,        可选,  指定配置文件(cfg.ini)"
         echo "  t,  node_id,         可选,  指定节点"
         echo "  m,  max_active,      可选,  最大并发数"
@@ -114,7 +112,7 @@ if [ x"$MAX_ACTIVE" = x ] ; then
 fi
 
 #处理账期
-if [ $SETT_MONTH -eq -1 ]; then
+if [ "$SETT_MONTH" = "-1" ]; then
 	year=`date +"%Y"`
 	month=`date +"%m" `
 	let "lastMonth=month-1"
@@ -126,7 +124,7 @@ if [ $SETT_MONTH -eq -1 ]; then
 	fi
 	SETT_MONTH=$year$lastMonth
 fi
-if [ $SETT_MONTH -eq 0 ]; then
+if [ "$SETT_MONTH" = "0" ]; then
 	SETT_MONTH=`date +"%Y%m"`
 fi
 
@@ -135,39 +133,39 @@ fi
 if [ "$DB_ON" = "Y" ] ; then
     TMP_RULE_TYPE_CODE=`sqlplus -silent $CONNSTR <<EOF  
     SET PAGESIZE 0 FEEDBACK OFF VERIFY OFF HEADING OFF ECHO OFF  
-    Select Wmsys.Wm_Concat(Data_Id) ruleType From Rwd_Core_Static a Where a.Type_Id = 'RWD_RULE_TYPE_CODE';
+    Select Replace(Wmsys.Wm_Concat(Data_Id),',',':') ruleType From Rwd_Core_Static a Where a.Type_Id = 'RWD_RULE_TYPE_CODE';
     EXIT;  
 EOF`
 fi
 echo `date +"%Y-%m-%d %H:%M:%S"`" RULE_TYPE_CODE equals ${RULE_TYPE_CODE} ,use database =${DB_ON} , TMP_RULE_TYPE_CODE : "$TMP_RULE_TYPE_CODE >>$callLogFile
 
-if [ "$RULE_TYPE_CODE" -eq 0 ] ; then
+if [ "$RULE_TYPE_CODE" = "0" ] ; then
     if [ -z "$TMP_RULE_TYPE_CODE" ]; then
-        RULE_TYPE_CODE="4000,4110,4120,4130,4140,4150,4250"
+        RULE_TYPE_CODE="4000:4110:4120:4130:4140:4150:4250"
     else
         RULE_TYPE_CODE=$TMP_RULE_TYPE_CODE
     fi
 fi
-echo `date +"%Y-%m-%d %H:%M:%S"`" RULE_TYPE_CODE equals ${RULE_TYPE_CODE} ,use database =${DB_ON} , current RULE_TYPE_CODE : "$RULE_TYPE_CODE >>$callLogFile
+echo `date +"%Y-%m-%d %H:%M:%S"`" current RULE_TYPE_CODE : "$RULE_TYPE_CODE >>$callLogFile
 
 #从数据库读地州编码
 if [ "$DB_ON" = "Y" ] ; then
     TMP_EPARCHY_CODE=`sqlplus -silent $CONNSTR <<EOF  
     SET PAGESIZE 0 FEEDBACK OFF VERIFY OFF HEADING OFF ECHO OFF  
-    Select Wmsys.Wm_Concat(Area_Code) areaCode From Rwd_Core_Area a Where a.Area_Level = 20;  
+    Select Replace(Wmsys.Wm_Concat(Area_Code),',',':') areaCode From Rwd_Core_Area a Where a.Area_Level = 20;  
     EXIT;  
 EOF`
 fi
 echo `date +"%Y-%m-%d %H:%M:%S"`" EPARCHY_CODE equals ${EPARCHY_CODE} ,use database =${DB_ON} , TMP_EPARCHY_CODE : "$TMP_EPARCHY_CODE >>$callLogFile
 
-if [ "$EPARCHY_CODE" -eq 0 ]; then
+if [ "$EPARCHY_CODE" = "0" ]; then
     if [ -z "$TMP_EPARCHY_CODE" ]; then
-        EPARCHY_CODE="0770,0771,0772,0773,0774,0775,0776,0777,0778,0779,0780,0781,0782,0783"
+        EPARCHY_CODE="0770:0771:0772:0773:0774:0775:0776:0777:0778:0779:0780:0781:0782:0783"
     else
         EPARCHY_CODE=$TMP_EPARCHY_CODE
     fi
 fi
-echo `date +"%Y-%m-%d %H:%M:%S"`" EPARCHY_CODE equals ${EPARCHY_CODE} ,use database =${DB_ON} , current EPARCHY_CODE : "$EPARCHY_CODE >>$callLogFile
+echo `date +"%Y-%m-%d %H:%M:%S"`" current EPARCHY_CODE : "$EPARCHY_CODE >>$callLogFile
 
 #有节点时需要拼-t
 if [ x"${NODE_ID}" = x ]; then
@@ -189,44 +187,45 @@ echo `date +"%Y-%m-%d %H:%M:%S"`"-----------------------------------------------
 echo `date +"%Y-%m-%d %H:%M:%S"`"------------------------------------all biz begin----------------startTime="$startTime >>$callLogFile
 echo `date +"%Y-%m-%d %H:%M:%S"`"-------------------------------------------------------------------------------------" >>$callLogFile
 
-arrEparchyCodes=(${EPARCHY_CODE//,/ })
-arrRuleTypeCodes=(${RULE_TYPE_CODE//,/ })
-
-#shell 程序日志
-programLogFile=${LOG_PATH}/${PROGRAM_NAME}_${SETT_MONTH}_${startTimeStr}.log
-if [ -f $programLogFile ]; then
-        rm $programLogFile
-fi
+arrEparchyCodes=(${EPARCHY_CODE//:/ })
+arrRuleTypeCodes=(${RULE_TYPE_CODE//:/ })
 
 #切换到引擎目录
-cd $RWD_HOME/bin
+#cd $RWD_HOME/bin
+cd $SHELL_HOME
 
 #开始处理
 for ruleTypeCode in ${arrRuleTypeCodes[@]}; do
         for eparchyCode in ${arrEparchyCodes[@]}; do
-                AMOUNT=` ps -ef |grep "$PROGRAM_NAME" | grep -v $0 | grep -v grep |sort -u |wc -l `;
-                AMOUNT2=` ps -ef |grep "$PROGRAM_NAME" | grep -v $0 | grep "$eparchyCode" |grep -v grep |sort -u |wc -l `;
-                #echo "ps -ef,AMOUNT="${AMOUNT}",AMOUNT2="${AMOUNT2}",MAX_ACTIVE="${MAX_ACTIVE}
+                #程序详细日志
+                programLogFile=${LOG_PATH}/${PROGRAM_NAME}_${eparchyCode}_${ruleTypeCode}_${startTimeStr}.log
+                if [ -f $programLogFile ]; then
+                    rm $programLogFile
+                fi
+
+                AMOUNT=` ps -ef |grep executeEng | grep -v $0 | grep -v grep |sort -u |wc -l `;
+                AMOUNT2=` ps -ef |grep executeEng | grep -v $0 | grep "$eparchyCode" |grep -v grep |sort -u |wc -l `;
+                echo `date +"%Y-%m-%d %H:%M:%S"`" ps -ef ,AMOUNT="${AMOUNT}",AMOUNT2="${AMOUNT2}",MAX_ACTIVE="${MAX_ACTIVE} >> $callLogFile
                 while [ "$AMOUNT" -ge ${MAX_ACTIVE} ] || [ "$AMOUNT2" -eq 1 ]; do
-                    #echo "sleep ..."
+                    echo `date +"%Y-%m-%d %H:%M:%S"`" sleep ..." >> $callLogFile
                     sleep 10
-                    AMOUNT=` ps -ef |grep "$PROGRAM_NAME" | grep -v $0 | grep -v grep |sort -u |wc -l `;
-                    AMOUNT2=` ps -ef |grep "$PROGRAM_NAME" | grep -v $0 | grep "$eparchyCode" |grep -v grep |sort -u |wc -l `;
-                    #echo "ps -ef ,AMOUNT="${AMOUNT}",AMOUNT2="${AMOUNT2}",MAX_ACTIVE="${MAX_ACTIVE}
+                    AMOUNT=` ps -ef |grep executeEng | grep -v $0 | grep -v grep |sort -u |wc -l `;
+                    AMOUNT2=` ps -ef |grep executeEng | grep -v $0 | grep "$eparchyCode" |grep -v grep |sort -u |wc -l `;
+                    echo `date +"%Y-%m-%d %H:%M:%S"`" in while ps -ef ,AMOUNT="${AMOUNT}",AMOUNT2="${AMOUNT2}",MAX_ACTIVE="${MAX_ACTIVE} >> $callLogFile
                 done
                 sleep 10
-                echo " begin execute $PROGRAM_NAME,param[sett_month|eparchy_code|rule_type_code|node_id|max_active|config_file|other_params]" >> $callLogFile
-                echo "                                 ["$SETT_MONTH"|"$eparchyCode"|"$ruleTypeCode"|"$NODE_ID"|"$MAX_ACTIVE"|"$CONFIG_FILE"|"$OTHER_PARAMS"]" >> callLogFile
+                echo `date +"%Y-%m-%d %H:%M:%S"`" begin execute executeEng.sh,param[sett_month|eparchy_code|rule_type_code|node_id|max_active|config_file|other_params]" >> $callLogFile
+                echo `date +"%Y-%m-%d %H:%M:%S"`"                                 ["$SETT_MONTH"|"$eparchyCode"|"$ruleTypeCode"|"$NODE_ID"|"$MAX_ACTIVE"|"$CONFIG_FILE"|"$OTHER_PARAMS"]" >> $callLogFile
                 #程序调用开始
-                echo " ./"$PROGRAM_NAME" -i"${CONFIG_FILE}" -c"${SETT_MONTH}" -e"${eparchyCode}" -r"${ruleTypeCode}" "${NODE_PARAM}" -"${OTHER_PARAMS} >> $callLogFile
-                ./$PROGRAM_NAME -i${CONFIG_FILE} -c${SETT_MONTH} -e${eparchyCode} -r${ruleTypeCode} ${NODE_PARAM} -${OTHER_PARAMS} >> $programLogFile
+                echo `date +"%Y-%m-%d %H:%M:%S"`" ./executeEng.sh "$PROGRAM_NAME" -i"${CONFIG_FILE}" -c"${SETT_MONTH}" -e"${eparchyCode}" -r"${ruleTypeCode}" "${NODE_PARAM}" -"${OTHER_PARAMS} >> $callLogFile
+                ./executeEng.sh $PROGRAM_NAME -i${CONFIG_FILE} -c${SETT_MONTH} -e${eparchyCode} -r${ruleTypeCode} ${NODE_PARAM} -${OTHER_PARAMS} >> $programLogFile &
         done
 done
 
-AMOUNT=` ps -ef |grep "$PROGRAM_NAME" | grep -v $0 |grep -v grep |sort -u |wc -l `;
+AMOUNT=` ps -ef |grep executeEng | grep -v $0 |grep -v grep |sort -u |wc -l `;
 while [ "$AMOUNT" -ge 1 ]; do
         sleep 10
-        AMOUNT=` ps -ef |grep "$PROGRAM_NAME" | grep -v $0 |grep -v grep |sort -u |wc -l `;
+        AMOUNT=` ps -ef |grep executeEng | grep -v $0 |grep -v grep |sort -u |wc -l `;
 done
 
 endTime=`date +%s`
